db1e7ea7a266e83db8e8ab0ce2d1011cd35b6622
Abhinav Chaturvedi <achaturvedi@calance.com>
2020-04-25 14:07:08 +0530

    Add book and author model

--- book_store/models.py
+++ book_store/models.py
@@ -6,3 +6,26 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, index=True)
     phone = db.Column(db.String(10), unique=True, index=True)
     password_hash = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    preview_url = db.Column(db.String(200))
+    description = db.Column(db.String(500))
+    pages = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    books = db.relationship('Book', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
--- book_store/routes.py
+++ book_store/routes.py
@@ -1,7 +1,21 @@
 from flask import render_template
 from book_store import app
+from book_store.models import User
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/register')
+def register():
+    pass
