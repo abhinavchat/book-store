9874e4ece97b68ed5c85c2cb12ce59eaad1c26bd
Abhinav Chaturvedi <achaturvedi@calance.com>
2020-04-30 11:32:37 +0530

    Refactor config on the basis of envs

--- book_store/__init__.py
+++ book_store/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-from config import Config
+from config import create_config
+import os
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(create_config[os.getenv('ENVIRONMENT') or 'dev'])
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -3,5 +3,20 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY =  os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'books.db')
+    DEBUG=False
 
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'books.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+class TestConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'test_books.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+class ProductionConfig(Config):
+    pass
+
+
+create_config = dict(dev=DevelopmentConfig, test=TestConfig, prod=ProductionConfig)
