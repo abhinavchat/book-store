9a66c904d35d8e5c7c2bcc3ef6ad7ee14556da1d
Abhinav Chaturvedi <achaturvedi@calance.com>
2020-04-30 16:44:18 +0530

    Filter results by title and authors

--- book_store/static/js/main.js
+++ book_store/static/js/main.js
@@ -11,10 +11,10 @@ var app = (function(){
 
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tdTitle = tr[i].getElementsByTagName("td")[1];
+            tdAuthor = tr[i].getElementsByTagName("td")[3];
+            if (tdTitle || tdAuthor) {
+            if (tdTitle.textContent.toUpperCase().indexOf(filter) > -1 || tdAuthor.textContent.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
